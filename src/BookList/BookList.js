179accdcd9e7b619f3c0086fe498009dfbb61f4e
Harvey Le <harveyxcle@gmail.com>
2020-04-02 11:08:19 -0400

    updated started over

---
+++ src/BookList/BookList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class BookList extends Component {
+  render() { 
+    return ( 
+        <div className='bookList'>
+          <ul>
+            <li>Hello</li>
+          </ul>
+        </div>
+     )
+  }
+}
+ 
+export default BookList
