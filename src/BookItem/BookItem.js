f639b129d9705b26b3c34eb6f208daa7d38ac53f
Harvey Le <harveyxcle@gmail.com>
2020-04-02 13:25:57 -0400

    updated bookitem

--- src/BookItem/BookItem.js
+++ src/BookItem/BookItem.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 
 class BookItem extends Component {
+  constructor(props) {
+    super(props)
+    this.setState = {
+
+    }
+  }
   render() { 
     return ( 
         <li>
-          
+          <h3>{this.props.title}</h3>
         </li>
      );
   }
