33202df9c610c2f28fea0e803ea7924420af2be8
Harvey Le <harveyxcle@gmail.com>
2020-04-02 13:21:57 -0400

    Added BookList

--- src/App.js
+++ src/App.js
@@ -5,16 +5,38 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      
+      currentSearch: []
     }
   }
 
-  
+  componentDidMount() {
+    const url = 'https://www.googleapis.com/books/v1/volumes?q=brandon+sanderson&key=AIzaSyA1rowpyT_Ye7N3wZngsMfSMnGv9Z-fQv8'
+    fetch(url)
+      .then(res => {
+        if(!res.ok) {
+          throw new Error('Something went wrong, please try again')
+        }
+        return res;
+      })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        this.setState({
+          currentSearch: data,
+          error: null
+        })
+      })
+      .catch(err => {
+        this.setState({
+          error: err.message
+        })
+      })
+  }
 
   render() {
     return (
       <main className='App'>
-        <BookList />
+        <BookList items={this.state.currentSearch}/>
       </main>
     )
   }  
--- src/BookList/BookList.js
+++ src/BookList/BookList.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
+import BookItem from './BookItem/BookItem'
 
 class BookList extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
   render() { 
+    const listItem = this.props.items.map((list, index) => (
+      <BookItem title={list.items.items[index].volumeInfo.title} />
+    ));
     return ( 
         <div className='bookList'>
           <ul>
-            <li>Hello</li>
+            {listItem}
           </ul>
         </div>
      )
